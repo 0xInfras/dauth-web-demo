3fd4e75765c15bfbde294ab4445d8b8059bca104
irvin518 <irvin518@163.com>
2023-07-19 10:01:23 +0800

    fix tx ui

--- src/components/TestDAuthWeb.vue
+++ src/components/TestDAuthWeb.vue
@@ -1,28 +1,41 @@
+<!-- eslint-disable vue/valid-template-root -->
 <template>
-  <div class="test">
+  <div>
+    <div class="TestDAuthWeb1">
     <button @click="startAuth">
       发起Twitter登录授权
     </button>
-    <label>{{ addressText }}</label>
   </div>
+  <div class="TestDAuthWeb2">
+    <label>address:</label>
+    <label ref="addLabel">{{ addressText }}</label>
+  </div>
+  <div class="TestDAuthWeb3">
+    <label>转账给</label>
+    <input type="text" v-model="transferAddress" placeholder="转出地址"></input>
+    <label>0.001eth</label>
+    <button @click="transfer">
+      提交
+    </button>
+  </div>
+  <div class="TestDAuthWeb4">
+    <label>{{ trxRes }}</label>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import {DAuthWalletManaget} from "dauth-web"
+import { Component, Vue } from 'vue-property-decorator';
+import {DAuthWalletManaget} from "dauth-web";
 
 @Component
-export default class TestUndo extends Vue {
-  name = 'TestDAuthWeb'
+export default class TestDAuthWeb extends Vue{
 
-  data() {
-    return {
-      addressText: 'address:'
-    }
-  }
-  
+   addressText : string = "地址"
+   transferAddress : string = ""
+   trxRes : string = "交易结果"
 
-  private startAuth() {
+    startAuth() {
     const info = {
       type: 'TWITTER',
       clientId: 'Vks1X3E3WVZoTHpXUUx3RGhaNlU6MTpjaQ',
@@ -31,25 +44,52 @@ export default class TestUndo extends Vue {
     DAuthWalletManaget.loginWithType(info)
   }
 
-  mounted(){
-    DAuthWalletManaget.initSDK({appId: "1bfe5bbf619681e49cdc62d07badc4cb",
-    appKey: "4466*dff@cde",
-    signKey:"2342a&*&45aeq",
-    sdkVersion:"1.2.2",
-    urlForAppServerKeyToSubmit:""});
-    this.checkAuth()
+   transfer(){
+
+    if ("" !== this.addressText){
+     DAuthWalletManaget.excute(
+        this.transferAddress,
+        0.001,
+        '0x'
+      ).then((res)=>{
+        this.trxRes = res;
+      }).catch((res)=>{
+        this.trxRes = res;
+      });
+    }
+
   }
 
-  private async checkAuth() {
+  mounted(){
+    DAuthWalletManaget.initSDK({appId: "1bfe5bbf619681e49cdc62d07badc4cb",
+    signKey:"2342a&*&45aeq",
+    sdkVersion:"1.2.2",
+    serverTag:"test",
+    urlForAppServerKeyToSubmit:""});
+    this.checkAuth();
+  }
+
+   async checkAuth() {
     const response =  await DAuthWalletManaget.checkLoginRedirctUrl({});
     //window.alert(JSON.stringify(response))
     if( response.error === 0 ){
-      DAuthWalletManaget.createWallet({ dauthId: response.data.dauthId,
-          access_token:response.data.dauthAccessToken}).then(
+      const dauthReponse = {
+        dauthId: response.data.dauthId,
+        access_token:response.data.dauthAccessToken
+      }
+      
+      DAuthWalletManaget.queryWalletAddress(dauthReponse).then((res)=>{
+        this.addressText = res.data;
+      }
+      ).catch((res)=>{
+        console.log(res);
+        DAuthWalletManaget.createWallet(dauthReponse).then(
             (res:any)=>{
-              window.alert(res.data);
+              console.log(res);
+             this.addressText = res.data;
             }
           );
+      });
     }
   }
 }
